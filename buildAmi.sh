bfaddad0dd498d98f19d7075d118c6f353c54e95
AnkitPatro <ankitpatro009@gmail.com>
2020-06-28 16:08:04 -0400

    Code Deploy Installation

--- buildAmi.sh
+++ buildAmi.sh
@@ -1,3 +1,6 @@
+echo 'ubuntu version'
+sudo lsb_release -a
+
 echo 'cleaning cache'
 sudo apt clean all
 sudo apt install wget
@@ -5,7 +8,20 @@ sudo apt install wget
 
 echo "Installing JDK-8"
 sudo apt-get update
-sudo apt install openjdk-8-jre-headless -y
+sudo apt-get install openjdk-8-jre-headless -y
 java -version
 echo "<  <  <  JDK-8 installed successfully   >  >  >"
+
+
+echo "Installing code-deploy-agent"
+sudo apt-get update
+sudo apt-get install ruby -y
+sudo apt-get install wget
+cd /home/ubuntu
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+echo "codedeploy-agent status"
+# sudo service codedeploy-agent status
+
  
