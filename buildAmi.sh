cf90fe5be1fccbb35d730660090d70b3b523f77b
AnkitPatro <ankitpatro009@gmail.com>
2020-07-05 16:40:43 -0400

    Reverting changes

--- buildAmi.sh
+++ buildAmi.sh
@@ -8,8 +8,8 @@ sudo apt install wget
 
 echo "Installing JDK-8"
 sudo apt-get update
-# sudo apt-get install openjdk-8-jre-headless -y
-sudo apt install -t jessie-backports  openjdk-8-jre-headless ca-certificates-java
+sudo apt-get install openjdk-8-jre-headless -y
+# sudo apt install -t jessie-backports  openjdk-8-jre-headless ca-certificates-java
 java -version
 echo "<  <  <  JDK-8 installed successfully   >  >  >"
 
